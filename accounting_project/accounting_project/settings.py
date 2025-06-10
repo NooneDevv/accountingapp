a6231f986c583914864b11cb372888be69365e7d
Tommy <49360278+NooneDevv@users.noreply.github.com>
2025-06-10 18:34:23 +0200

    static root

--- accounting_project/accounting_project/settings.py
+++ accounting_project/accounting_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tnisz=h%cyn83gof4-uq7dw#_&!+)o(lhwz#(e!n+com16gdnq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '127.0.0.1', 'localhost']
 
 
 # Application definition
